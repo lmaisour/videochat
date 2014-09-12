4f7d115b5e899abd9aa3a87cb8d98fb1d9ee769d
lmaisour <lmaisour@gmail.com>
2014-09-11 21:38:55 -0700

    running into error undefined method inject

---
+++ app/controllers/rooms_controller.rb
@@ -0,0 +1,54 @@
+class RoomsController < ApplicationController
+	before_action :config_opentok, :except => [:index]
+
+	def index
+		@rooms = Room.where(public: true)
+		@new_room = Room.new
+	end
+
+	def create
+		session = @opentok.create_session request.remote_addr
+		params[:room][:sessionId] = session.session_id
+		
+		# session_id = session.session_id
+		# binding.pry
+		# request.remote_addr
+		# params[:room][:sessionId] = session.session_id
+
+		@new_room = Room.new(room_params)
+
+		if @new_room.save
+			redirect_to("/show/"+@new_room.id.to_s) 
+		else
+			redirect_to root_path 
+		end
+
+	end
+	
+	def show
+		# begin
+			@room = Room.find(params[:id])
+			binding.pry
+			@tok_token = @opentok.generate_token :session_id =>@room.sessionId
+			binding.pry
+		# rescue
+		# 	flash[:notice] = "some shit went wrong son"
+		# 	redirect_to :action => :index
+		# end
+	end
+
+	private
+
+	def config_opentok
+		if @opentok.nil?
+		 		@opentok = OpenTok::OpenTok.new "44979352", "c4b975eed97c2f85866bdc289904ddb84a842a56"
+		end
+	end
+
+	def room_params
+    params.require(:room).permit(:sessionId, :name, :public)
+  end
+
+
+end
+
